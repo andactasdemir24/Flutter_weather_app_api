622e7691242fa9e9595d2614c74cc685a3ea0cb6
andactasdemir24 <96003255+andactasdemir24@users.noreply.github.com>
2023-04-08 16:16:44 +0300

    replace city select button with searchh bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -19,7 +19,7 @@ class _HomePageState extends State<HomePage> {
   final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey = GlobalKey<RefreshIndicatorState>();
   //arama cubugu controllerı
   final TextEditingController searchController = TextEditingController();
-  //uygulama açılınca gösterilen şehirr
+  //uygulama açılınca gösterilen şehirrr
   String searchTerm = 'Ankara';
 
   //Kontrolleri yap
